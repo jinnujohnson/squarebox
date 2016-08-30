b56d17fe575a61aff1ee51750d870bf94732a6e3
jinnudeveloper <jinnudeveloper@gmail.com>
2016-08-30 20:39:46 +0530

    Error for size=1 corrected

--- squarebox/squarebox.cpp
+++ squarebox/squarebox.cpp
@@ -8,24 +8,27 @@ int main()
 	int size;
 	cout << "Enter Box Size\n";
 	cin >> size;
+
 	for(int col=0;col<size;++col)
 		{
 			cout << "#";
 		}
-	cout << "\n";
-	for(int row=0;row<size-2;++row)
+	if(size > 1) 
 		{
-			cout<< "#";
-		for(int col=0;col<size-2;++col)
-			{
-				cout << " ";
-			}
-			cout<<"#\n";
-		}
-	for(int col=0;col<size;++col)
-		{
-			cout << "#";
-		}
+			cout << "\n";
+			for(int row=0;row<size-2;++row)
+				{
+					cout<< "#";
+					for(int col=0;col<size-2;++col)
+						{
+							cout << " ";
+						}
+					cout<<"#\n";
+				}
+			for(int col=0;col<size;++col)
+				{
+					cout << "#";
+				}
 	return 0;
 }
 
